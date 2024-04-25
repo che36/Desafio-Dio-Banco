ea59ea8aedede9394d170a2750eebb7c1a0214e9
Neo40 <tron36.cc@gmail.com>
2024-04-25 18:02:58 -0300

    sobrescrita de metodos da interface,encapsulamento de atributos e adição de método para impressão de dados da Conta

--- src/Conta.java
+++ src/Conta.java
@@ -1,4 +1,4 @@
-public abstract class Conta {
+public abstract class Conta implements IConta {
 
     private static final int AGENCIA_PADRAO = 1;
     private static int SEQUENCIAL = 1;
@@ -6,37 +6,47 @@ public abstract class Conta {
     protected int agencia;
     protected int numero;
     protected double saldo;
-    protected CLiente cliente;
+    protected Cliente cliente;
 
 
-    public Conta(CLiente cliente) {
+    public Conta(Cliente cliente) {
         this.agencia = Conta.AGENCIA_PADRAO;
         this.numero = SEQUENCIAL++;
         this.cliente = cliente;
     }
 
-    public int getAgencia() {
-        return agencia;
+    @Override
+    public void sacar(double valor) {
+        saldo -= valor;
     }
 
-    public void setAgencia(int agencia) {
-        this.agencia = agencia;
+    @Override
+    public void transferir(double valor, IConta contaDestino) {
+        this.sacar(valor);
+        contaDestino.depositar(valor);
     }
 
-    public int getNumero() {
-        return numero;
-    }
-
-    public void setNumero(int numero) {
-        this.numero = numero;
+    @Override
+    public void depositar(double valor) {
+        saldo += valor;
     }
 
     public double getSaldo() {
         return saldo;
     }
 
-    public void setSaldo(double saldo) {
-        this.saldo = saldo;
+    public int getNumero() {
+        return numero;
     }
 
+    public int getAgencia() {
+        return agencia;
+    }
+
+    protected void imprimirInfosComuns() {
+        System.out.println(String.format("Titular: $s", this.cliente.getNome()));
+        System.out.println(String.format("Agencia: %d", this.agencia));
+        System.out.println(String.format("Numero: %d", this.numero));
+        System.out.println(String.format("Saldo: %.2f", this.saldo));
+    }
 }
