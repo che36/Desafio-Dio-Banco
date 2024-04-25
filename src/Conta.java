634146b763a57c5d49dddc200aec54a0c5d7c385
Neo40 <tron36.cc@gmail.com>
2024-04-24 23:47:24 -0300

    Adição de classe Conta com atributos,construtor e getter and setter para encaspsulamento

---
+++ src/Conta.java
@@ -0,0 +1,42 @@
+public abstract class Conta {
+
+    private static final int AGENCIA_PADRAO = 1;
+    private static int SEQUENCIAL = 1;
+
+    protected int agencia;
+    protected int numero;
+    protected double saldo;
+    protected CLiente cliente;
+
+
+    public Conta(CLiente cliente) {
+        this.agencia = Conta.AGENCIA_PADRAO;
+        this.numero = SEQUENCIAL++;
+        this.cliente = cliente;
+    }
+
+    public int getAgencia() {
+        return agencia;
+    }
+
+    public void setAgencia(int agencia) {
+        this.agencia = agencia;
+    }
+
+    public int getNumero() {
+        return numero;
+    }
+
+    public void setNumero(int numero) {
+        this.numero = numero;
+    }
+
+    public double getSaldo() {
+        return saldo;
+    }
+
+    public void setSaldo(double saldo) {
+        this.saldo = saldo;
+    }
+
+}
